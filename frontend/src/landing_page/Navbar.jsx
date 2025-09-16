6ad62f9559f282bd1730f7c68821ea16f78c3dd4
Sunil Kumar <148389457+sunil998877@users.noreply.github.com>
2025-09-16 21:52:30 +0530

    Update Navbar.jsx

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <nav className="navbar navbar-expand-lg    fixed-top  "  >
             <div className="container  p-2 "  >
              <Link to={"/"}>
-                 <img src="public/media/logo.svg " alt="" className="fixed " style={{width:"140px"}}  />
+                 <img src="\media\logo.svg " alt="" className="fixed " style={{width:"140px"}}  />
              </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
