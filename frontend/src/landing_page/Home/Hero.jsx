c5db031886e739b08bd7714bdf8b785454a59340
sunil kumar <your_registered_github_email@example.com>
2025-09-19 01:12:18 +0530

    add button center

--- frontend/src/landing_page/Home/Hero.jsx
+++ frontend/src/landing_page/Home/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
                 <h1 className="mt-3 fs-3 opacity text-center" >Invest in everything</h1>
                 <p className="fs-5 mt-1 text-muted text-center">Online plateform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
                 <Link to={"/signup"}  >
-                <button className=" pl-2 pr-2 btn btn-color  opacity   fs-5 mt-4 rounded-1   custom-hover " style={{ width: "180px" }} >Sign up Now</button>
+                <button className=" pl-2 pr-2 btn btn-color  opacity text-center   fs-5 mt-4 rounded-1   custom-hover " style={{ width: "180px" }} >Sign up Now</button>
                 </Link>
             </div>
         </div>
