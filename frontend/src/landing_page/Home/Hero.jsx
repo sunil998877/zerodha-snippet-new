45a3adef36fbdc46dd95f4a5bed81de69b055b3c
sunil kumar <your_registered_github_email@example.com>
2025-09-19 00:03:30 +0530

    add Link

--- frontend/src/landing_page/Home/Hero.jsx
+++ frontend/src/landing_page/Home/Hero.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 function Hero() {
-    const Navigate = useNavigate();
+
     return ( 
         <div className="container text-md-center     Hero">
             <div className="row d-flex justify-content-center mt-2 ">
                 <img src="\media\homeHero.png" alt="Hero Image" className="mb-5 img-fluid" style={{width:"45rem"}}  />
                 <h1 className="mt-3 fs-3 opacity text-center" >Invest in everything</h1>
                 <p className="fs-5 mt-1 text-muted text-center">Online plateform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button className=" pl-2 pr-2 btn btn-color  opacity   fs-5 mt-4 rounded-1   custom-hover " style={{ width: "180px" }} Navigate={"/signup"}>Sign up Now</button>
+                <Link to={"/signup"}  >
+                <button className=" pl-2 pr-2 btn btn-color  opacity   fs-5 mt-4 rounded-1   custom-hover " style={{ width: "180px" }} >Sign up Now</button>
+                </Link>
             </div>
         </div>
      );
